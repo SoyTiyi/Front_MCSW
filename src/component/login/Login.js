2fae04ae9773066ab1fa2413c5bc051a7cd81c58
nduran06 <natalia.duran@mail.escuelaing.edu.co>
2021-02-13 13:10:25 -0500

    arreglo post

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -7,6 +7,9 @@ const Login = (props) => {
 
     const history = useHistory();
 
+    const[username, setUsername] = useState("");
+    const[password, setPassword] = useState("");
+
     const routeChange = () => {
         let path = '/register';
         history.push(path);
@@ -17,23 +20,53 @@ const Login = (props) => {
         history.push(path);
     }
 
+    const postLogin = () => {
+        console.log(username,password);
+
+        var data = new FormData();
+        data.append('usuario', username);
+        data.append('passwd', password);
+
+        axios.post('http://localhost:8000/login.php', data)
+        .then(response => {
+            console.log(response)
+
+            if(response.data !== null){
+
+              let rol = response.data.trim();
+
+              if(rol === 'admin') {
+                let path = '/overdraft';
+                history.push(path);
+              }
+
+              else if (rol === 'cliente'){
+                let path = '/summary';
+                history.push(path);
+              }
+
+            }
+        })
+        .catch(error => `Error: ${error}`);
+    }
+
     return (
         <Container>
-            <form action="http://localhost:8000/login.php" method="post">
-                <Title>
-                    <h1>Login</h1>
-                </Title>
-                <Row>
-                    <label>Username: </label>
-                    <input type="text" name="usuario"/>
-                </Row>
-                <Row>
-                    <label>password: </label>
-                    <input type="text" name="passwd"/>
-                </Row>
-                <Button type="submit" onClick={goToSummary}>Login</Button>
-                <Button onClick={routeChange}>Register</Button>
-            </form>
+          <Title>
+              <h1>Login</h1>
+          </Title>
+          <Row>
+              <label>Username: </label>
+              <input type="text" name="username" onChange={event => setUsername(event.target.value)}/>
+          </Row>
+          <Row>
+              <label>Password: </label>
+              <input type="password" name="pasword" onChange={event => setPassword(event.target.value)}/>
+          </Row>
+          <Buttons>
+              <Button onClick={postLogin}>Login</Button>
+              <Button onClick={routeChange}>Register</Button>
+          </Buttons>
         </Container>
     );
 }
